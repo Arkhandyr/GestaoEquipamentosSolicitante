c9c92258be8f4a5d4e4dc3e469a7d19c4d39f7d2
Victor Muniz <victor.hm2000@aluno.ifsc.edu.br>
2021-05-07 16:39:35 -0300

    Versão final

---
+++ GestaoEquipamentos.ConsoleApp/Telas/TelaSolicitante.cs
@@ -0,0 +1,137 @@
+﻿using GestaoEquipamentos.ConsoleApp.Controladores;
+using GestaoEquipamentos.ConsoleApp.Dominio;
+using System;
+
+namespace GestaoEquipamentos.ConsoleApp.Telas
+{
+    class TelaSolicitante : TelaBase
+    {
+        private ControladorSolicitante controladorSolicitante;
+
+        public TelaSolicitante(ControladorSolicitante controlador)
+            : base("Cadastro de Solicitantes")
+        {
+            controladorSolicitante = controlador;
+        }
+
+        public override void InserirNovoRegistro()
+        {
+            ConfigurarTela("Inserindo um novo solicitante...");
+
+            bool conseguiuGravar = GravarSolicitante(0);
+
+            if (conseguiuGravar)
+                ApresentarMensagem("Solicitante inserido com sucesso", TipoMensagem.Sucesso);
+            else
+            {
+                ApresentarMensagem("Falha ao tentar inserir o solicitante", TipoMensagem.Erro);
+                InserirNovoRegistro();
+            }
+        }
+
+        public override void ExcluirRegistro()
+        {
+            ConfigurarTela("Excluindo um solicitante...");
+
+            VisualizarRegistros();
+
+            Console.WriteLine();
+
+            Console.Write("Digite o número do solicitante que deseja excluir: ");
+            int idSelecionado = Convert.ToInt32(Console.ReadLine());
+
+            bool conseguiuExcluir = controladorSolicitante.ExcluirSolicitante(idSelecionado);
+
+            if (conseguiuExcluir)
+                ApresentarMensagem("Solicitante excluído com sucesso", TipoMensagem.Sucesso);
+            else
+            {
+                ApresentarMensagem("Falha ao tentar excluir o solicitante", TipoMensagem.Erro);
+                ExcluirRegistro();
+            }
+        }
+
+        public override void EditarRegistro()
+        {
+            ConfigurarTela("Editando um solicitante...");
+
+            VisualizarRegistros();
+
+            Console.WriteLine();
+
+            Console.Write("Digite o ID do solicitante que deseja editar: ");
+            int idSelecionado = Convert.ToInt32(Console.ReadLine());
+
+            bool conseguiuEditar = GravarSolicitante(idSelecionado);
+
+            if (conseguiuEditar)
+                ApresentarMensagem("Solicitante editado com sucesso", TipoMensagem.Sucesso);
+            else
+            {
+                ApresentarMensagem("Falha ao tentar editar o solicitante", TipoMensagem.Erro);
+                EditarRegistro();
+            }
+        }
+
+        public override void VisualizarRegistros()
+        {
+            ConfigurarTela("Visualizando solicitantes...");
+
+            MontarCabecalhoTabela();
+
+            Solicitante[] solicitantes = controladorSolicitante.SelecionarTodosSolicitantes();
+
+            if (solicitantes.Length == 0)
+            {
+                ApresentarMensagem("Nenhum chamado registrado!", TipoMensagem.Atencao);
+                return;
+            }
+
+            foreach (Solicitante solicitante in solicitantes)
+            {
+                Console.WriteLine("{0,-10} | {1,-30} | {2,-55} | {3,-25}",
+                    solicitante.id, solicitante.chamado.titulo, solicitante.email, solicitante.telefone);
+            }
+        }
+
+        #region Métodos Privados
+        private static void MontarCabecalhoTabela()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("{0,-10} | {1,-30} | {2,-55} | {3,-25}", "Id", "Nome", "E-Mail", "Telefone");
+
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------");
+
+            Console.ResetColor();
+        }
+
+        private bool GravarSolicitante(int id)
+        {
+            string resultadoValidacao;
+            bool conseguiuGravar = true;
+
+            Console.Write("Digite o nome do solicitante: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o email do solicitante: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Digite o telefone do solicitante: ");
+            string telefone = Console.ReadLine();
+
+            resultadoValidacao = controladorSolicitante.RegistrarSolicitante(
+                id, nome, email, telefone);
+
+            if (resultadoValidacao != "SOLICITANTE_VALIDO")
+            {
+                ApresentarMensagem(resultadoValidacao, TipoMensagem.Erro);
+                conseguiuGravar = false;
+            }
+
+            return conseguiuGravar;
+        }
+
+        #endregion
+    }
+}
